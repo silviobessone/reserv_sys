51bace1af1312ab1e88c093d6a9b7c38e823c478
German Lugo <german.lugo@bitnibs.com>
2018-01-03 21:19:56 +0100

    Delete uneeded comment.

--- app.py
+++ app.py
@@ -1,24 +1,11 @@
 from flask import Flask, render_template, make_response
 from flask_restful import Resource, Api
-from model.dbase import db
 
 
 app = Flask(__name__)
 
 api = Api(app)
 
-"""TODO: Use the properly form of pony orm""" 
-# @app.before_request
-# def database_connect():
-#     if db.is_closed():
-#         database.connect()
-
-
-# @app.teardown_request
-# def database_disconnect(response):
-#     if not database.is_closed():
-#         db.close()
-#     return response
 
 class home(Resource):
     def get(self):
@@ -32,7 +19,9 @@ class template_test(Resource):
                 "my_string": "Chocolate!",
                 "my_list": [7, 4, 8, 6, 1, 5, 3, 0, 2, 9]
                 }
-        return make_response(render_template('template.html', **data), 200, header)
+        return make_response(render_template(
+               'template.html', **data), 200, header
+               )
 
 api.add_resource(home, "/")
 api.add_resource(template_test, "/test")
